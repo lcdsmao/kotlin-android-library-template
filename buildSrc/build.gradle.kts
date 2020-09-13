dd54d572306ed48d8a542945395ba1c90ea041de
Mao <lcdsmao@gmail.com>
2020-09-13 15:47:04 +0900

    Add bintray publish plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,9 +10,11 @@ repositories {
 dependencies {
   implementation(GradlePlugin.kotlin)
   implementation(GradlePlugin.android)
+  implementation(GradlePlugin.bintray)
 }
 
 object GradlePlugin {
   const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:_"
   const val android = "com.android.tools.build:gradle:_"
+  const val bintray = "com.jfrog.bintray.gradle:gradle-bintray-plugin:_"
 }
---
+++ buildSrc/src/main/java/bintray-publish-config.gradle.kts
@@ -0,0 +1,34 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+import java.util.Date
+
+plugins {
+  id("com.jfrog.bintray")
+}
+
+bintray {
+  user = System.getProperty("bintrayUser")
+  key = System.getProperty("bintrayKey")
+  setPublications("maven")
+
+  pkg(closureOf<BintrayExtension.PackageConfig> {
+    repo = "maven"
+    userOrg = "user-org" // TODO modify this
+    name = extra["POM_ARTIFACT_ID"].toString()
+    vcsUrl = extra["POM_SCM_URL"].toString()
+    websiteUrl = extra["POM_URL"].toString()
+    setLicenses("Apache-2.0")
+    version(closureOf<BintrayExtension.VersionConfig> {
+      name = project.version.toString()
+      released = Date().toString()
+    })
+  })
+}
+
+project.afterEvaluate {
+  tasks.named("publishMavenPublicationToMavenLocal") {
+    // Let bintray publication depend on artifact tasks created by 'com.vanniktech.maven.publish'
+    tasks.getByName("publishMavenPublicationToLocalRepository").dependsOn.forEach {
+      dependsOn(it)
+    }
+  }
+}
