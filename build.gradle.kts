acde51fd8494a12018a39a8e788bcda47dd0e7a4
Mao <lcdsmao@gmail.com>
2020-11-07 11:27:23 +0900

    Remove versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 plugins {
   id("io.gitlab.arturbosch.detekt")
-  id("com.github.ben-manes.versions")
   id("com.diffplug.spotless")
   id("org.jetbrains.dokka")
   id("com.vanniktech.maven.publish") apply false
@@ -50,11 +47,3 @@ subprojects {
 tasks.dokkaHtmlMultiModule.configure {
   outputDirectory.set(rootDir.resolve("docs/api"))
 }
-
-tasks.withType<DependencyUpdatesTask> {
-  rejectVersionIf {
-    isNonStable(candidate.version)
-  }
-}
-
-fun isNonStable(version: String) = "^[0-9,.v-]+(-r)?$".toRegex().matches(version).not()
