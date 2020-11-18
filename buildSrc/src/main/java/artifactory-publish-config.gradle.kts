9d71bf10838c9c1592684b63994a01bd6aebcb9f
Mao <lcdsmao@gmail.com>
2020-11-18 21:41:25 +0900

    Fix artifactory task, remove reslove, add deps

--- buildSrc/src/main/java/artifactory-publish-config.gradle.kts
+++ buildSrc/src/main/java/artifactory-publish-config.gradle.kts
@@ -1,19 +1,13 @@
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
+
 plugins {
   id("com.jfrog.artifactory")
+  `maven-publish`
 }
 
 artifactory {
   setContextUrl("https://oss.jfrog.org/artifactory")
   withGroovyBuilder {
-    "resolve" {
-      "repository" {
-        setProperty("repoKey", "libs-release")
-        setProperty("username", System.getProperty("artifactoryUser"))
-        setProperty("password", System.getProperty("artifactoryPassword"))
-        setProperty("maven", true)
-      }
-    }
-
     "publish" {
       "repository" {
         if (project.version.toString().endsWith("-SNAPSHOT")) {
@@ -30,6 +24,15 @@ artifactory {
       "defaults" {
         "publications"("maven")
       }
+    }
+  }
+}
+
+project.afterEvaluate {
+  tasks.withType<ArtifactoryTask> {
+    publishing.publications.forEach {
+      val publishTaskName = "publish${it.name.capitalize()}PublicationToLocalRepository"
+      dependsOn(publishTaskName)
     }
   }
 }
